397de2b02e976830bd1af41f22732b650510eaf1
Mohamed Elsaid <Mohamed_Elsaid@aol.com>
2024-04-22 04:35:30 -0400

    fixed
    fixed howdy

--- microservice/server_test.go
+++ microservice/server_test.go
@@ -15,7 +15,7 @@ var (
 // Unit Tests
 
 func TestHandler(t *testing.T) {
-	expected := []byte("Hello World!\n")
+	expected := []byte("Howdy World!\n")
 	req, err := http.NewRequest("GET", buildUrl("/"), nil)
 	if err != nil {
 		t.Fatal(err)
