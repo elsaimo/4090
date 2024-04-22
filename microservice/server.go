f62300e6e002c56f7ce868c0155ea12aa0c8508f
Mohamed Elsaid <Mohamed_Elsaid@aol.com>
2024-04-22 04:32:51 -0400

    server
    change server.go to be Howdy World

--- microservice/server.go
+++ microservice/server.go
@@ -16,5 +16,5 @@ func NewServer(host string, port string) *http.Server {
 	}
 }
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello World!\n")
+	fmt.Fprintf(w, "Howdy World!\n")
 }
